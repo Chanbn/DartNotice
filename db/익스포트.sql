8758506a84702f38b47ec3e8c9032267473bf2f8
Chanbn <alioila@naver.com>
2025-09-05 16:37:29 +0900

    2

---
+++ db/익스포트.sql
@@ -0,0 +1,141 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 금요일-9월-05-2025   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table DART_REPORT
+--------------------------------------------------------
+
+  CREATE TABLE "NOTICEDATA"."DART_REPORT" 
+   (	"RECPT_NO" VARCHAR2(255 CHAR), 
+	"CORP_NAME" VARCHAR2(255 CHAR), 
+	"RECPT_DT" DATE, 
+	"REPOTR_NM" VARCHAR2(255 CHAR)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into NOTICEDATA.DART_REPORT
+SET DEFINE OFF;
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250718000104','자이가이스트',to_date('25/07/18','RR/MM/DD'),'동일인등출자계열회사와의상품ㆍ용역거래변경');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250718800108','동부건설',to_date('25/07/18','RR/MM/DD'),'[기재정정]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250718900105','빅텍',to_date('25/07/18','RR/MM/DD'),'[기재정정]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250718800095','HJ중공업',to_date('25/07/18','RR/MM/DD'),'[기재정정]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250718000088','E1물류',to_date('25/07/18','RR/MM/DD'),'동일인등출자계열회사와의상품ㆍ용역거래변경');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250718000042','상방공원피에프브이',to_date('25/07/18','RR/MM/DD'),'동일인등출자계열회사와의상품ㆍ용역거래변경');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250718900024','나노',to_date('25/07/18','RR/MM/DD'),'단일판매ㆍ공급계약체결(자율공시)              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250718800022','금양',to_date('25/07/18','RR/MM/DD'),'[기재정정]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250718000021','이지전선',to_date('25/07/18','RR/MM/DD'),'동일인등출자계열회사와의상품ㆍ용역거래변경');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250717800558','제이준코스메틱',to_date('25/07/17','RR/MM/DD'),'타법인주식및출자증권처분결정              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250717900511','비에이치아이',to_date('25/07/17','RR/MM/DD'),'[기재정정]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250717800533','코오롱',to_date('25/07/17','RR/MM/DD'),'단일판매ㆍ공급계약체결(자회사의 주요경영사항)              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250717800515','코오롱',to_date('25/07/17','RR/MM/DD'),'[기재정정]단일판매ㆍ공급계약체결(자회사의 주요경영사항)              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250717800512','코오롱글로벌',to_date('25/07/17','RR/MM/DD'),'단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250717800500','코오롱글로벌',to_date('25/07/17','RR/MM/DD'),'[기재정정]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250717900469','중앙첨단소재',to_date('25/07/17','RR/MM/DD'),'단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250717900477','SG',to_date('25/07/17','RR/MM/DD'),'[기재정정]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250717000351','육일씨엔에쓰',to_date('25/07/17','RR/MM/DD'),'[기재정정]주요사항보고서(타법인주식및출자증권양도결정)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200220800099','삼호개발',to_date('20/02/20','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200205900533','남화토건',to_date('20/02/05','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200319900448','바이오니아',to_date('20/03/19','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200326900563','아미코젠',to_date('20/03/26','RR/MM/DD'),'[첨부추가]타법인주식및출자증권취득결정(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200304900188','링크제니시스',to_date('20/03/04','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200319900532','힘스',to_date('20/03/19','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200311900302','힘스',to_date('20/03/11','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200117900448','바이오톡스텍',to_date('20/01/17','RR/MM/DD'),'[첨부추가]타법인주식및출자증권처분결정');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200110000851','한주에이알티',to_date('20/01/10','RR/MM/DD'),'[첨부추가]주요사항보고서(타법인주식및출자증권양수결정)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200406900332','바이오니아',to_date('20/04/06','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200929900244','동신건설',to_date('20/09/29','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200701900516','유니셈',to_date('20/07/01','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20200914900163','성우테크론',to_date('20/09/14','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20201223900192','솔트룩스',to_date('20/12/23','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20201214900253','선익시스템',to_date('20/12/14','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20201113901360','인베니아',to_date('20/11/13','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210309800229','삼호개발',to_date('21/03/09','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210115900067','비에이치아이',to_date('21/01/15','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210126900116','삼영이엔씨',to_date('21/01/26','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210219900156','우정바이오',to_date('21/02/19','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210223900093','솔트룩스',to_date('21/02/23','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210325901554','싸이토젠',to_date('21/03/25','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210420900116','AP위성',to_date('21/04/20','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210607900358','일승',to_date('21/06/07','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210430900414','이지케어텍',to_date('21/04/30','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210407900406','링크제니시스',to_date('21/04/07','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210622900071','솔트룩스',to_date('21/06/22','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210525800291','SG글로벌',to_date('21/05/25','RR/MM/DD'),'[첨부추가]타법인주식및출자증권취득결정');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210624000301','에스케이텔레시스',to_date('21/06/24','RR/MM/DD'),'[첨부추가]주요사항보고서(타법인주식및출자증권양도결정)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210721800122','HS화성',to_date('21/07/21','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210812900306','알에스오토메이션',to_date('21/08/12','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210712900368','이큐셀',to_date('21/07/12','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210812900077','바이브컴퍼니',to_date('21/08/12','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210819600462','타임기술',to_date('21/08/19','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20210930900613','덕우전자',to_date('21/09/30','RR/MM/DD'),'[첨부추가]타법인주식및출자증권취득결정(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20211201800153','한화오션',to_date('21/12/01','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20211109900248','아스플로',to_date('21/11/09','RR/MM/DD'),'[첨부추가]투자판단관련주요경영사항(소재부품기술개발(패키지형)산업기술혁신사업 선정)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20211227900489','HLB',to_date('21/12/27','RR/MM/DD'),'[첨부추가]타법인주식및출자증권취득결정');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20211231000478','에코바이브',to_date('21/12/31','RR/MM/DD'),'[첨부추가]주요사항보고서(중요한자산양수도결정(기타))');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220210900345','신원종합개발',to_date('22/02/10','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220308900183','제이엔케이글로벌',to_date('22/03/08','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220128900716','GST',to_date('22/01/28','RR/MM/DD'),'단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220214900439','이랜텍',to_date('22/02/14','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220104900375','아이티센엔텍',to_date('22/01/04','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220525900144','바른손',to_date('22/05/25','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220628900202','케일럼',to_date('22/06/28','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220421800656','하이스틸',to_date('22/04/21','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220630000991','위메이드',to_date('22/06/30','RR/MM/DD'),'[첨부추가]주요사항보고서(타법인주식및출자증권양도결정)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220615000402','THE CUBE&',to_date('22/06/15','RR/MM/DD'),'[첨부추가]주요사항보고서(타법인주식및출자증권양도결정)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220705900360','아이티센엔텍',to_date('22/07/05','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220823900068','신원종합개발',to_date('22/08/23','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20220823900071','신원종합개발',to_date('22/08/23','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20221114900853','코세스',to_date('22/11/14','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20221006900093','신원종합개발',to_date('22/10/06','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20221219900477','THE CUBE&',to_date('22/12/19','RR/MM/DD'),'[첨부추가]타법인주식및출자증권취득결정(자율공시)(종속회사의주요경영사항)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20221216901051','티사이언티픽',to_date('22/12/16','RR/MM/DD'),'[첨부추가]타법인주식및출자증권취득결정');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20230320900304','코세스',to_date('23/03/20','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20230306900111','지앤비에스 에코',to_date('23/03/06','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20230614900217','성우테크론',to_date('23/06/14','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20230403900181','코세스',to_date('23/04/03','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20230727900931','자이글',to_date('23/07/27','RR/MM/DD'),'[첨부추가]타법인주식및출자증권취득결정');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20230724900203','포시에스',to_date('23/07/24','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20230901900240','드림어스컴퍼니',to_date('23/09/01','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20230829900236','셀레믹스',to_date('23/08/29','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결(자율공시)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20231030900349','아바코',to_date('23/10/30','RR/MM/DD'),'단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20231212900675','스마트레이더시스템',to_date('23/12/12','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20231005900180','자람테크놀로지',to_date('23/10/05','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20240319901058','필에너지',to_date('24/03/19','RR/MM/DD'),'단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20240207900390','넥써쓰',to_date('24/02/07','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20240321900205','켄코아에어로스페이스',to_date('24/03/21','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20240221002237','삼진엘앤디',to_date('24/02/21','RR/MM/DD'),'[첨부추가]주요사항보고서(타법인주식및출자증권양도결정)');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20240612900108','케이엔에스',to_date('24/06/12','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20240425900666','벨로크',to_date('24/04/25','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20240625900284','케이엔에스',to_date('24/06/25','RR/MM/DD'),'[첨부추가]타법인주식및출자증권취득결정(자율공시)              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20240724900162','케이엔에스',to_date('24/07/24','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20240902900485','스마트레이더시스템',to_date('24/09/02','RR/MM/DD'),'[첨부추가]단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20240913800436','SIMPAC',to_date('24/09/13','RR/MM/DD'),'타법인주식및출자증권취득결정              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20241030900373','영우디에스피',to_date('24/10/30','RR/MM/DD'),'단일판매ㆍ공급계약체결(자율공시)              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250718900619','한국첨단소재',to_date('25/07/18','RR/MM/DD'),'타법인주식및출자증권취득결정              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250718800568','삼성물산',to_date('25/07/18','RR/MM/DD'),'단일판매ㆍ공급계약체결              ');
+Insert into NOTICEDATA.DART_REPORT (RECPT_NO,CORP_NAME,RECPT_DT,REPOTR_NM) values ('20250718800524','HDC랩스',to_date('25/07/18','RR/MM/DD'),'[기재정정]단일판매ㆍ공급계약체결              ');
+--------------------------------------------------------
+--  DDL for Index SYS_C007385
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "NOTICEDATA"."SYS_C007385" ON "NOTICEDATA"."DART_REPORT" ("RECPT_NO") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  Constraints for Table DART_REPORT
+--------------------------------------------------------
+
+  ALTER TABLE "NOTICEDATA"."DART_REPORT" MODIFY ("RECPT_NO" NOT NULL ENABLE);
+  ALTER TABLE "NOTICEDATA"."DART_REPORT" ADD PRIMARY KEY ("RECPT_NO")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS"  ENABLE;
